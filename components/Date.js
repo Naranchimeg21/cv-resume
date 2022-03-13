972d57d926511b588f0bf6b14384ae0603402ba7
Naranchimeg21 <kisetsu.ss2@gmail.com>
2022-03-14 00:10:02 +0800

    Create Date.js

---
+++ components/Date.js
@@ -0,0 +1,23 @@
+// import React from "react"
+
+
+// const DateComp = (props) => {
+//     let style = {
+//         'width': '20px',
+//         'height':'20px' ,
+//         'border-radius':'50%', 
+//         'border' : '1px solid black'
+//     }
+
+//     return (
+//         <div>
+//             <div style={style} style={{props.active == active ? "backgroundColor" : "black" : backgroundColor : transparent}}></div>
+//             <div style={style}></div>
+//             <div style={style}></div>
+//             <div style={style}></div>
+//             <div style={style}></div>
+//         </div>
+//     )
+// }
+
+// export default DateComp
